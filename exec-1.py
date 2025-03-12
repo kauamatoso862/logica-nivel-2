fce3396e48ddca8f6897b366459b4f51ac7c2200
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-11 22:46:56 -0300

    exec-1.py

---
+++ exec-1.py
@@ -0,0 +1,10 @@
+# Escreva um algoritmo que armazene o valor 10 em uma variável A e o valor 20 em uma variável B. A seguir (utilizando apenas atribuições entre variáveis) troque os seus conteúdos fazendo com que o valor que está em A passe para B e vice-versa. Ao final, escrever os valores que ficaram armazenados nas variáveis.
+
+A = 10
+B = 20
+
+A, B = B, A
+
+print("Após a troca:")
+print("A =", A)
+print("B =",B)
