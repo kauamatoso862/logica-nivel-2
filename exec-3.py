121555242eec9ca60438289e4cc62eb41ae0b983
kauamatoso862 <66929853+kauamatoso862@users.noreply.github.com>
2025-03-12 08:56:13 -0300

    exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,7 @@
+# Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+
+numero=int(input("Digite um número: "))
+
+antecessor = numero - 1
+
+print("O antecessor do número é:", antecessor)
